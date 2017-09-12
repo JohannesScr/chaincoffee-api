469eb4f824d4f47518beed56b2fb2958657d9c79
Johannes Scribante <johannesscr@Johanness-MacBook.local>
2017-09-12 22:35:48 +0200

    set up all routes with hard coded responses, tested routes in Postman and they are all working

--- app.js
+++ app.js
@@ -3,10 +3,13 @@
 let express = require('express');   // import express
 let app = express();    // define the express instance
 let jsonParser = require('body-parser').json;   // import body-parser's json parser
+let logger = require('morgan');
 
 // import router
 let routes = require('./routes');
 
+// logger will give us status code for our api responses
+app.use(logger('dev'));
 // parse the request.body object to json an make is accessible from req.body
 app.use(jsonParser());
 
@@ -19,6 +22,10 @@ app.use((req, res, next) => {
   console.log('first piece of middleware');
   next();   // if an argument is passed to next, it goes straight to the error handler
 });
+
+
+
+
 
 
 
--- routes.js
+++ routes.js
@@ -37,7 +37,18 @@ router.post('/user', (req, res) => {
 // route to get a specific user
 router.get('/user/:uid', (req, res) => {
   res.json({
-    response: 'You sent a /GET to /chaincoffee/user/' + req.params.uid
+    response: 'You sent a /GET to /chaincoffee/user/' + req.params.uid,
+    user_id: req.params.uid
+  });
+});
+
+// /PUT /chaincoffee/user/:uid
+// route to get a specific user
+router.put('/user/:uid', (req, res) => {
+  res.json({
+    response: 'You sent a /PUT to /chaincoffee/user/' + req.params.uid,
+    user_id: req.params.uid,
+    body: req.body
   });
 });
 
@@ -45,7 +56,66 @@ router.get('/user/:uid', (req, res) => {
 // route to remove a new user
 router.delete('/user/:uid', (req, res) => {
   res.json({
-    response: 'You sent a /DELETE to /chaincoffee/user/' + req.params.uid
+    response: 'You sent a /DELETE to /chaincoffee/user/' + req.params.uid,
+    user_id: req.params.uid
+  });
+});
+
+// /GET /chaincoffee/event
+// route to get all the events
+router.get('/event', (req, res) => {
+  res.json({
+    response: 'You sent a /GET request to /chaincoffee/event'
+  });
+});
+
+// /POST /chaincoffee/event
+// route to create a new event
+router.post('/event', (req, res) => {
+  res.json({
+    response: 'You sent a /POST request to /chaincoffee/event',
+    body: req.body
+  });
+});
+
+// /GET /chaincoffee/event/:eid
+// route to get a specific event
+router.get('/event/:eid', (req, res) => {
+  res.json({
+    response: 'You sent a /GET request to /chaincoffee/event/' + req.params.eid,
+    event_id: req.params.eid
+  });
+});
+
+// /PUT /chaincoffee/event/:eid
+// route to update a specific event
+router.put('/event/:eid', (req, res) => {
+  res.json({
+    response: 'You sent a /PUT request to /chaincoffee/event/' + req.params.eid,
+    event_id: req.params.eid,
+    body: req.body
+  });
+});
+
+// /DELETE /chaincoffee/event/:eid
+// route to get a specific event
+router.get('/event/:eid', (req, res) => {
+  res.json({
+    response: 'You sent a /DELETE request to /chaincoffee/event/' + req.params.eid,
+    event_id: req.params.eid
+  });
+});
+
+// /POST /chaincoffee/event/:eid/event-quote
+// /POST /chaincoffee/event/:eid/event-cancel
+// /POST /chaincoffee/event/:eid/event-confirm
+// route to update a specific event's status
+router.post('/event/:eid/event-:status', (req, res) => {
+  res.json({
+    response: 'You sent a /PUT request to /chaincoffee/event/' + req.params.eid + '/event-' + req.params.status,
+    event_id: req.params.eid,
+    event_status: req.params.status,
+    body: req.body
   });
 });
 
